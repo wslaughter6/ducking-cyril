64b1aaed70297061c7f6498553fe35f39cff1f88
willard <will@360works.com>
2013-02-18 16:15:54 -0500

    Minor change to engine

--- Aether_Windows/Aether/headers/AetherEngine.h
+++ Aether_Windows/Aether/headers/AetherEngine.h
@@ -5,3 +5,19 @@
 //  Created by William Slaughter on 2/18/13.
 //  Copyright (c) 2013 Grimalkin Games. All rights reserved.
 //
+#include <iostream>
+#include "stdio.h"
+#include "SDL.h"
+#include "SDL_image.h"
+
+class AetherEngine {
+    SDL_Surface *mainScreen;
+    //SDL_Surface *image;
+    //SDL_Surface *readyImage;
+    int blitResult;
+public:
+    AetherEngine(SDL_Surface *srcScreen);
+    ~AetherEngine(void);
+    void fadeIn();
+    void fadeOut();
+};
