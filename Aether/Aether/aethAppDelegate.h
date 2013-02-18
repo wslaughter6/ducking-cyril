beb3e4eea4e7c61d50c1a50969c3c946cc3b3d9e
willard <luminiferousaethergame@gmail.com>
2013-02-17 19:46:45 -0500

    Created code base and xcode projects

---
+++ Aether/Aether/aethAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  aethAppDelegate.h
+//  Aether
+//
+//  Created by William Slaughter on 2/17/13.
+//  Copyright (c) 2013 Grimalkin Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface aethAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Aether/Aether/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Aether
+//
+//  Created by William Slaughter on 2/17/13.
+//  Copyright (c) 2013 Grimalkin Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
